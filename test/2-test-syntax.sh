1a34045b7072e8b2be20ed81b3267c54705240d7
alexandre <alexandremr01@gmail.com>
2024-06-17 23:57:51 +0100

    fix tests

--- test/2-test-syntax.sh
+++ test/2-test-syntax.sh
@@ -1,5 +1,5 @@
-./bin/sclc test/2-test-syntax.in output > test/2-test-syntax-output.out
-if ! cmp -s test/2-test-syntax-output.out test/2-test-syntax-expected-output.txt; then
+./bin/sclc test/2-test-syntax.in test/2-test-syntax.out > test/2-test-syntax.out.txt
+if ! cmp -s test/2-test-syntax.out.txt test/2-test-syntax-expected-output.txt; then
   echo "Output does not match expected"
   exit 1
 fi
--- test/3-test-semantic.sh
+++ test/3-test-semantic.sh
@@ -1,5 +1,5 @@
-./bin/sclc test/3-test-semantic.in output > test/3-test-semantic-output.out
-if ! cmp -s test/3-test-semantic-output.out test/3-test-semantic-expected-output.txt; then
+./bin/sclc test/3-test-semantic.in test/3-test-semantic.out > test/3-test-semantic.out.txt
+if ! cmp -s test/3-test-semantic.out.txt test/3-test-semantic-expected-output.txt; then
   echo "Output does not match expected"
   exit 1
 fi
